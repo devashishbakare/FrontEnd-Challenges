d290d0f0abb64825a5ac11466c220487649beb8e
devashish bakare <devashishbakare@gmail.com>
2023-02-10 08:47:49 +0530

    added some shortcut for ajax request

--- Dog-API-Ajax/script.js
+++ Dog-API-Ajax/script.js
@@ -1,4 +1,7 @@
-console.log("hey");
+/*
+ With using xmlHttpRequest
+
+ 
 function getImageFromApi(){
     let xhr = new XMLHttpRequest();
 
@@ -8,5 +11,34 @@ function getImageFromApi(){
         $('#dog-img').attr('src', data.message);
     };
     xhr.send();
+}
+$('#fetch-button').click(getImageFromApi);
+
+*/
+
+/*
+
+    //with using jquery
+
+    function getImageFromApi(){
+        $.ajax({
+            type: "GET",
+            url: "https://dog.ceo/api/breeds/image/random",
+            success : function(data){
+                $('#dog-img').attr('src', data.message);
+            }
+        });
+        
+    }
+    $('#fetch-button').click(getImageFromApi);
+
+*/
+
+//shortcut for get request
+function getImageFromApi(){
+   $.get('https://dog.ceo/api/breeds/image/random', function(data){
+    $('#dog-img').attr('src', data.message);
+   });
+    
 }
 $('#fetch-button').click(getImageFromApi);
